c52602deb11950272ae04dbe99f01ba2e207a3eb
elenore29 <elenore.nava@gmail.com>
2019-04-04 19:51:31 -0600

    Retornando el resultado de la función para validar los url

---
+++ print.js
@@ -0,0 +1,7 @@
+var colors = require('colors');
+
+const print = (result) => {
+    console.log(colors.blue(result))
+}
+
+module.exports = print;
--- validate.js
+++ validate.js
@@ -1,21 +1,47 @@
-const validateUrl = (findUrl) => {
-  findUrl.forEach(element => {
-    if (element.match(/(https:\/\/[^\s]+)/g)) {
-      const https = require('https');
-      https.get(element, (res) => {
-        console.log('status', res.statusCode, element)
-      }).on('error', (res) => {
-        console.error('status', res.port, element, 'not found');
-      })
-    } else {
-      const http = require('http');
-      http.get(element, (res) => {
-        console.log('status', res.statusCode, element)
-      }).on('error', (res) => {
-        console.error('status', res.port, element, 'not found');
-      })
-    }
-  });
+const https = require('https');
+const http = require('http');
+var colors = require('colors');
+
+const validateUrl = async (findUrl) => {
+  const myPromise = new Promise((resolve, reject) => {
+    const validateArr = [];
+    let listo = 0;
+
+    findUrl.forEach(element => {
+      if (element.match(/(https:\/\/[^\s]+)/g)) {
+        https.get(element, (res) => {
+          validateArr.push('status' + ' ' + res.statusCode + " " + element)
+          listo += 1;
+          if (findUrl.length === listo) {
+            resolve(validateArr);
+          }
+        }).on('error', (res) => {
+          validateArr.push('Not found' + ' ' + res.port + " " + element)
+          listo += 1;
+          if (findUrl.length === listo) {
+            resolve(validateArr);
+          }
+        });
+      } else {
+        http.get(element, (res) => {
+          validateArr.push('status' + ' ' + res.statusCode + " " + element)
+          listo += 1;
+          if (findUrl.length === listo) {
+            resolve(validateArr);
+          }
+        }).on('error', (res) => {
+          validateArr.push('Not found' + ' ' + res.port + " " + element)
+          listo += 1;
+          if (findUrl.length === listo) {
+            resolve(validateArr);
+          }
+        });
+      }
+      //for
+    });
+  })
+  const result = await myPromise;
+  return result
 }
 
 module.exports = validateUrl;
